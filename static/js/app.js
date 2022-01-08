7ada15a6ad19d6c3138303284cc57e011741e2f1
smilesandsobs <nmlamperti@gmail.com>
2022-01-08 15:22:47 -0800

    Adding File Structure

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+//Declare a variable, tbody and use d3.select to tell JavaScript to look for the <tbody> tags in the HTML
+var tbody = d3.select("tbody");
+
